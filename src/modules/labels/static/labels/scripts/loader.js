8f3804e34f3a6e7a033965f56f1c717103760ab7
gidaszewski <gidaszewskifranco@gmail.com>
2024-10-21 22:01:47 +0200

    frontend-admin: fix loader script

--- src/modules/labels/static/labels/scripts/loader.js
+++ src/modules/labels/static/labels/scripts/loader.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const isPopup = window.location.search.includes('_popup=1');
     const form = document.querySelector('form');
     const saveBtn = document.querySelector('[name="_save"]');
     const spinner = document.createElement('div');
 
     spinner.className = 'loading-spinner';
     spinner.style.display = 'none';
-
     form.appendChild(spinner);
 
     saveBtn.addEventListener('click', function () {
         spinner.style.display = 'block';
+
+        if (isPopup) {
+            form.style.opacity = '0.5';
+        }
     });
 });
