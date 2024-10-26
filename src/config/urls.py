ccfc9d25fb69aea1d5bd4b6272ee4f1db04fd074
gidaszewski <gidaszewskifranco@gmail.com>
2024-10-26 15:30:25 +0200

    backend: fix reverse url signup form

--- src/config/urls.py
+++ src/config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path("dashboard/", admin.site.urls),
+    path("dashboard/", admin.site.urls, name="dashboard"),
     path("website/", include("modules.website.urls")),
     path("campaigns/", include("modules.campaigns.urls")),
     path("accounts/", include("modules.registration.urls")),
--- src/modules/registration/views.py
+++ src/modules/registration/views.py
@@ -5,5 +5,5 @@ from django.views import generic
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("dashboard:login")
+    success_url = reverse_lazy("dashboard")
     template_name = "registration/signup.html"
