33f120489cbd574557f56e1e4243ba97586108e6
gidaszewski <gidaszewskifranco@gmail.com>
2024-10-28 15:36:03 +0100

    implement success signup page

--- src/modules/registration/urls.py
+++ src/modules/registration/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, success_signup
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("signup/success", success_signup, name="signup-success"),
 ]
--- src/modules/registration/views.py
+++ src/modules/registration/views.py
@@ -1,9 +1,29 @@
 from .forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.models import Group
+from django.shortcuts import render
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("admin:index")
+    success_url = reverse_lazy("signup-success")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # User notification
+
+        # User config
+        user = self.object
+        user.is_staff = True
+        label_manager_group, created = Group.objects.get_or_create(name="Label Manager")
+        user.groups.add(label_manager_group)
+        user.save()
+
+        return response
+
+
+def success_signup(request):
+    return render(request, "registration/success_signup.html")
