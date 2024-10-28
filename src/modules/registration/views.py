fecc53d35854fdd77f818a1dbef3106789988115
gidaszewski <gidaszewskifranco@gmail.com>
2024-10-28 10:47:22 +0100

    backend: fix sign up success_url

--- src/modules/registration/views.py
+++ src/modules/registration/views.py
@@ -5,5 +5,5 @@ from django.views import generic
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("dashboard:index")
+    success_url = reverse_lazy("admin:index")
     template_name = "registration/signup.html"
